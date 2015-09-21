f9dd997e6c11b4e09568c3a6e8d8c2ecd84d3f0b
bjcobb <bjcobb@svsu.edu>
2015-09-21 15:39:05 -0400

    bjcobb: CIS Page

--- app/src/main/java/edu/svsu/cs403/cs403_fa2015/bjcobb_chap1.java
+++ app/src/main/java/edu/svsu/cs403/cs403_fa2015/bjcobb_chap1.java
@@ -5,14 +5,22 @@ import android.support.v7.app.AppCompatActivity;
 import android.text.method.ScrollingMovementMethod;
 import android.util.Log;
 import android.view.Menu;
+import android.webkit.WebView;
 import android.widget.TextView;
 
-public class gpcorser_chap2 extends AppCompatActivity {
+public class bjcobb_chap1 extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_gpcorser_chap2);
+        setContentView(R.layout.activity_bjcobb_chap1);
+        String html = getResources().getString(R.string.CIS_Web);
+        String mime = "text/html";
+        String encoding = "utf-8";
+
+        WebView myWebView = (WebView)this.findViewById(R.id.webView);
+        myWebView.getSettings().setJavaScriptEnabled(true);
+        myWebView.loadDataWithBaseURL(null, html, mime, encoding, null);
         Log.d("DEBUG", "onCreate was just called!");
     }
 
